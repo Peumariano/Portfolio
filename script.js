209fcf1b741ff0735e4740898f91064c8ad9fbc9
Peumariano <pedromariano747@gmail.com>
2023-04-27 00:30:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const desiredElement = document.getElementById('navbar'); // elemento alvo
+const pixelsAmount = '100'; // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > pixelsAmount) {
+    	desiredElement.classList.add('changeStyle'); // adiciona classe "changeColor"
+    } else {
+      desiredElement.classList.remove('changeStyle'); // remove classe "changeColor"
+    }
+});
